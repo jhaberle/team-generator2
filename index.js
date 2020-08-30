77e4e10b4c3009484ae8c12674e73d2c0f9b5cb2
Jake Haberle <jake.haberle@gmail.com>
2020-08-29 22:16:03 -0600

    added userprompt function with questions

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+  return inquirer.prompt([
+    {
+        type: "checkbox",
+        message: "choose position",
+        name: "position",
+        choices: [
+            "Manager",
+            "Developer",
+            "Intern"
+        ]
+    },
+    {
+        type: "input",
+        message: "employee name",
+        name: "name",
+    },
+    {
+        type: "input",
+        message: "employee email address",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "employee githud id",
+        name: "github",
+    }
+]);
+}
